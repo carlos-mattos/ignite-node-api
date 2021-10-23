b4dfe5acbc5c86f9591f7f92bd7ec0a475e4c9a5
Carlos Eduardo Andrade de Mattos <ceamattos.jf@gmail.com>
2021-10-23 15:17:39 -0300

    not able to insert two categories with same name

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -7,6 +7,14 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post("/", (req, res) => {
   const { name, description } = req.body;
 
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
+
+  if (categoryAlreadyExists) {
+    return res
+      .status(400)
+      .json({ error: "Essa categoria já existe no sistema" });
+  }
+
   categoriesRepository.create({ name, description });
 
   return res.status(201).send();
