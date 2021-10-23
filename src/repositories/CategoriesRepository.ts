bf290b5e8137ccdc32290897d8969d2ef0486445
Carlos Eduardo Andrade de Mattos <ceamattos.jf@gmail.com>
2021-10-23 15:09:15 -0300

    categories repository

---
+++ src/repositories/CategoriesRepository.ts
@@ -0,0 +1,22 @@
+import Category from "../model/Category";
+import { ICreateCategoryDTO } from "../dtos/CategoryDTO";
+
+export default class CategoriesRepository {
+  private categories: Category[];
+
+  constructor() {
+    this.categories = [];
+  }
+
+  create({ description, name }: ICreateCategoryDTO): void {
+    const newCategory = new Category();
+
+    Object.assign(newCategory, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.categories.push(newCategory);
+  }
+}
