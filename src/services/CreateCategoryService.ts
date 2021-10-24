6308deb045ebbbe0a0cedcdbdec9ec6257622184
Carlos Eduardo Andrade de Mattos <ceamattos.jf@gmail.com>
2021-10-24 09:48:13 -0300

    SRP

---
+++ src/services/CreateCategoryService.ts
@@ -0,0 +1,20 @@
+import CategoriesRepository from "../repositories/CategoriesRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+export default class CreateCategoryService {
+  constructor(private categoriesRepository: CategoriesRepository) {}
+
+  execute({ name, description }: IRequest): void {
+    const categoryAlreadyExists = this.categoriesRepository.findByName(name);
+
+    if (categoryAlreadyExists) {
+      throw new Error("Já existe uma categoria com esse nome.");
+    }
+
+    this.categoriesRepository.create({ name, description });
+  }
+}
