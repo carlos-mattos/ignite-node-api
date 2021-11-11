b9786bcdb4a48af1f9c70c27f61ab10cc2eb72a9
ceamattos <ceamattos.jf@gmail.com>
2021-11-11 00:05:59 -0300

    user migration

---
+++ src/modules/accounts/entities/User.ts
@@ -0,0 +1,35 @@
+import { v4 as uuid } from "uuid";
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
+
+@Entity("users")
+export default class User {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  driver_license: string;
+
+  @Column()
+  isAdmin: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
