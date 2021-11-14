48000accea23b7a2747d3851650c28bb5d0b25a1
ceamattos <ceamattos.jf@gmail.com>
2021-11-14 16:51:40 -0300

    route auth

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,56 @@
+import { inject, injectable } from "tsyringe";
+import IUsersRepository from "../../repositories/IUsersRepository";
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+
+// 1- usuario existe
+// 2- senha correta
+// 3- gerar o jwt
+
+@injectable()
+export default class AuthenticateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private userRepository: IUsersRepository
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.userRepository.findByEmail(email);
+
+    if (!user) {
+      throw new Error("Email ou senha incorretos");
+    }
+
+    const checkPassword = await compare(password, user.password);
+
+    if (!checkPassword) {
+      throw new Error("Email ou senha incorretos");
+    }
+
+    const token = sign({}, "4568902c4fe98c69c83f4e88a428964f", {
+      subject: user.id,
+      expiresIn: "1d",
+    });
+
+    return {
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+      token,
+    };
+  }
+}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,13 @@
+import "reflect-metadata";
 import { Router } from "express";
+import authenticateRoutes from "./authenticate.routes";
 import categoriesRoutes from "./categories.routes";
 import specificationsRoutes from "./specifications.routes";
 import usersRoutes from "./users.routes";
 
 const router = Router();
+
+router.use(authenticateRoutes);
 
 router.use("/categories", categoriesRoutes);
 router.use("/specifications", specificationsRoutes);
